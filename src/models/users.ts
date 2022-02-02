1e41e5afa17d60fb131210035bc43b24c0fe6a38
Ahmed Amin <ahmedamin1700@gmail.com>
2022-02-02 09:06:02 +0200

    Create authenticate function for users repository.

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,4 +1,4 @@
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import Client from '../database';
 
 export interface User {
@@ -46,6 +46,26 @@ class UsersRepository {
       return result.rows[0];
     } catch (error) {
       throw new Error(`Can't create user with this username ${username}`);
+    }
+  };
+
+  authenticate = async (username: string, password: string): Promise<User> => {
+    try {
+      const connection = await Client.connect();
+      const sql = 'SELECT * FROM users WHERE username=$1';
+
+      const result = await connection.query(sql, [username]);
+
+      // if user doesn't exists.
+      if (!result.rows[0]) throw new Error();
+
+      // if passwords doesn't match.
+      const valid = compare(password + process.env.PEPPER, result.rows[0].password);
+      if (!valid) throw new Error();
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Can't authenticate user with this username ${username}`);
     }
   };
 }
